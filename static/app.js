43a3b6be770c1b1a6c25706a94c031e474821ec5
Lizzy <alicia.s273596@cesurformacion.com>
2022-12-19 11:49:11 +0100

    Terminado

--- static/app.js
+++ static/app.js
@@ -1,3 +1,15 @@
-// nombre_del_alumno
-//
-// Completar JS aqui
+        /* Alicia Ruiznavarro Ambrona */
+var tabla = document.querySelector("#contenedor")
+var modal = document.querySelector("#contenedor2");
+var agregar = document.querySelector("#modalbtn");
+var volver = document.querySelector("#volver");
+
+agregar.onclick = function() {
+    tabla.style.display = "none";
+    modal.style.display = "block";
+}
+
+volver.onclick = function() {
+    modal.style.display = "none";
+    tabla.style.display = "block";
+}
